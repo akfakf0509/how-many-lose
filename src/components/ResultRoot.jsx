02c809f9f84e095e9f99dadb07640107ba788673
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2021-05-27 15:00:59 +0900

    Making Search Exception Case

--- src/components/ResultRoot.jsx
+++ src/components/ResultRoot.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import { faBackspace } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component } from "react";
+import store from "../store";
 import "./ResultRoot.css";
 import UserInfo from "./UserInfo";
 
-function ResultRoot() {
-  return (
-    <div className="result-root">
-      <h1>Why You Lose?</h1>
-      <div className="container">
-        <UserInfo />
+class ResultRoot extends Component {
+  getUserInfo() {
+    let currentContent = "";
+
+    if (store.getState().summoner.name) {
+      currentContent = <UserInfo />;
+    }
+
+    return currentContent;
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+
+            dispatch({ type: "SEARCH", summoner: null });
+          }}
+          className="btn-back"
+        >
+          <FontAwesomeIcon icon={faBackspace} size="3x" color="white" />
+        </button>
+        <div className="result-root">
+          <h1>Why You Lose?</h1>
+          <div className="container">{this.getUserInfo()}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ResultRoot;
