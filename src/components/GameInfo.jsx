ec6f1d4b491297ee98fd06e9c1f9d6d3a8e50e3b
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2021-06-28 20:07:30 +0900

    Improved gameinfo ui/ux

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -9,6 +9,7 @@ class GameInfo extends Component {
 
     this.state = {
       win: null,
+      currentSummoner: {},
       game: {},
     };
   }
@@ -27,7 +28,7 @@ class GameInfo extends Component {
         this.setState({ game: res });
         res.info.participants.forEach((e) => {
           if (summoner.name === e.summonerName) {
-            this.setState({ win: e.win });
+            this.setState({ win: e.win, currentSummoner: e });
           }
         });
       });
@@ -58,13 +59,69 @@ class GameInfo extends Component {
   }
 
   getGameContent() {
-    const { game } = this.state;
+    const { currentSummoner, game } = this.state;
 
     let currentComponent;
 
     if (game.info) {
+      const team1 = [];
+      const team2 = [];
+
+      const { participants } = game.info;
+
+      for (let i = 0; i < 10; i += 1) {
+        const isCurrentSummoner =
+          participants[i].summonerName === currentSummoner.summonerName;
+        currentComponent = <span>{participants[i].summonerName}</span>;
+        const currentComponent1 = (
+          <span className="game-content-container-kda">
+            <span className="game-content-kda">{participants[i].kills}</span>
+
+            <span className="game-content-contour">/</span>
+
+            <span className="game-content-kda game-content-deaths">
+              {participants[i].deaths}
+            </span>
+
+            <span className="game-content-contour">/</span>
+
+            <span className="game-content-kda">{participants[i].assists}</span>
+          </span>
+        );
+
+        if (i < 5) {
+          team1.push(
+            <li
+              className={
+                isCurrentSummoner ? "game-content-current-summoner" : ""
+              }
+              key={i}
+            >
+              {currentComponent}
+              {currentComponent1}
+            </li>
+          );
+        } else {
+          team2.push(
+            <li
+              className={
+                isCurrentSummoner ? "game-content-current-summoner" : ""
+              }
+              key={i}
+            >
+              {currentComponent1}
+              {currentComponent}
+            </li>
+          );
+        }
+      }
+
       currentComponent = (
-        <div className="game-content">{game.info.gameMode}</div>
+        <div className="game-content">
+          {game.info.gameMode}
+          <ul className="game-content-team game-content-team1">{team1}</ul>
+          <ul className="game-content-team game-content-team2">{team2}</ul>
+        </div>
       );
     } else {
       currentComponent = <div className="game-content">Content is empty</div>;
